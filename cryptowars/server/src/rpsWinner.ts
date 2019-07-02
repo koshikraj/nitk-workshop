47acbce0864bdc2e597f4d25dd5b295269b92fd0
koshikraj <koshik.raj@gmail.com>
2019-07-02 13:53:02 +0530

    updates client and server

---
+++ cryptowars/server/src/rpsWinner.ts
@@ -0,0 +1,21 @@
+interface RPSW {
+    [key:string]: string;
+}
+
+
+export const RockPaperScissorsGetLoser: RPSW = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper',
+}
+
+export const RockPaperScissorsGetWinner: RPSW = {
+    rock: 'paper',
+    paper: 'scissors',
+    scissors: 'rock',
+}
+
+export function getRandomWinner() {
+    const results: { [key: number]: string } = {0: 'rock', 1:'paper', 2:'scissors'};
+    return results[Math.floor(Math.random() * 3)];
+}
